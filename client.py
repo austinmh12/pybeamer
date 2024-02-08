8855f986530bb129814e3cf66b0f695aede12336
Austin Heil <austin.heil@saabinc.com>
2024-02-08 12:34:50 -0500

    User endpoints

--- client.py
+++ client.py
@@ -2,9 +2,11 @@ from __future__ import annotations
 from typing import Any
 
 from loguru import logger
+from math import ceil
 
 from .rest_client import RestClient
 from .projects import Project
+from .user import User
 
 class Codebeamer:
 	"""The Codebeamer API client"""
@@ -28,7 +30,7 @@ class Codebeamer:
 		TypeError — A type other than str or int was provided.
 		
 		Returns:
-		`Project` | — A project if one exists."""
+		`Project` — A project if one exists."""
 		if isinstance(project, str):
 			return self._get_project_by_name(project)
 		elif isinstance(project, int):
@@ -55,3 +57,69 @@ class Codebeamer:
 		if search['total'] == 0:
 			return
 		return Project(**search['projects'][0], client=self._client)
+
+	def get_users(self, page: int = 0, page_size: int = 25) -> list[User]:
+		"""Fetches all the users in the system.
+
+		Params:
+		page — The page number to fetch if you want a specific page of users. If 0 then all users are fetched. — int(0)
+		page_size — The number of results per page of users. Must be between 1 and 500. — int(25) 
+		
+		Returns:
+		list[`User`] — A list of users in the system."""
+		fetch_all = page == 0
+		if fetch_all:
+			page = 1
+		page_size = max(1, min(500, page_size)) # Clamp page_size between 1 and 500
+		params = {'page': page, 'pageSize': page_size}
+		users: list[User] = []
+		user_data = self._client.get('users', params=params)
+		total_pages = ceil(user_data['total'] / page_size)
+		users.extend([User(**u, client=self._client) for u in user_data['users']])
+		if fetch_all:
+			while params['page'] < total_pages:
+				params['page'] += 1
+				user_data = self._client.get('users', params=params)
+				users.extend([User(**u, client=self._client) for u in user_data['users']])
+		return users
+	
+	def get_user(self, user: str | int) -> User | None:
+		"""Fetches a specific user from the system.
+		
+		Params:
+		user — The name, email, or ID of the user to fetch. — str | int
+		
+		Raises:
+		TypeError — A type other than str or int was provided.
+		
+		Returns:
+		`User` — A user if one exists."""
+		if isinstance(user, str):
+			if '@' in user:
+				return self._get_user_by_email(user)
+			return self._get_user_by_name(user)
+		elif isinstance(user, int):
+			return self._get_user_by_id(user)
+		else:
+			raise TypeError(f'expected str or int, got {type(user)}')
+		
+	def _get_user_by_id(self, id: int) -> User | None:
+		try:
+			user = User(**self._client.get(f'users/{id}'), client=self._client)
+			return user
+		except:
+			return
+		
+	def _get_user_by_name(self, name: str) -> User | None:
+		try:
+			user = User(**self._client.get('users/findByName', params={'name': name}), client=self._client)
+			return user
+		except:
+			return
+		
+	def _get_user_by_email(self, email: str) -> User | None:
+		try:
+			user = User(**self._client.get('users/findByEmail', params={'email': email}), client=self._client)
+			return user
+		except:
+			return
---
+++ user.py
@@ -0,0 +1,201 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, Any
+
+from loguru import logger
+from datetime import datetime
+
+from .rest_client import RestClient
+
+class User:
+	"""Represents a user in codeBeamer."""
+	if TYPE_CHECKING:
+		_email: str | None
+		_first_name: str | None
+		_last_name: str | None
+		_title: str | None
+		_company: str | None
+		_address: str | None
+		_zip: int | None
+		_city: str | None
+		_state: str | None
+		_country: str | None
+		_date_format: str | None
+		_time_zone: str | None
+		_language: str | None
+		_phone: str | None
+		_skills: list[str] | None
+		_registry_date: datetime | None
+		_last_login_date: datetime | None
+		_status: str | None
+
+	def __init__(self, id: int, name: str, **kwargs):
+		self._id: int = id
+		self._name: str = name
+		self._email: str | None = kwargs.get('email') # Not present for system users
+		self._client: RestClient = kwargs.get('client')
+		# type only appears in GET /projects
+		if kwargs.get('type'):
+			# if type is present then no other information is present
+			self._first_name = None
+			self._last_name = None
+			self._title = None
+			self._company = None
+			self._address = None
+			self._zip = None
+			self._city = None
+			self._state = None
+			self._country = None
+			self._date_format = None
+			self._time_zone = None
+			self._language = None
+			self._phone = None
+			self._skills = None
+			self._registry_date = None
+			self._last_login_date = None
+			self._status = None
+		else:
+			self._first_name = kwargs.get('firstName')
+			self._last_name = kwargs.get('lastName')
+			self._title = kwargs.get('title')
+			self._company = kwargs.get('company')
+			self._address = kwargs.get('address')
+			self._zip = kwargs.get('zip')
+			self._city = kwargs.get('city')
+			self._state = kwargs.get('state')
+			self._country = kwargs.get('country')
+			self._date_format = kwargs.get('dateFormat')
+			self._time_zone = kwargs.get('timeZone')
+			self._language = kwargs.get('language')
+			self._phone = kwargs.get('phone')
+			self._skills = kwargs.get('skills')
+			self._registry_date = datetime.strptime(kwargs.get('registryDate'), '%Y-%m-%dT%H:%M:%S.%f')
+			self._last_login_date = datetime.strptime(kwargs.get('lastLoginDate'), '%Y-%m-%dT%H:%M:%S.%f')
+			self._status = kwargs.get('status')
+
+	@property
+	def id(self) -> int:
+		"""The ID of the user."""
+		return self._id
+
+	@property
+	def name(self) -> str:
+		"""The username of the user."""
+		return self._name
+
+	@property
+	def email(self) -> str | None:
+		"""The email of the user."""
+		return self._email
+
+	@property
+	def first_name(self) -> str | None:
+		"""The first name of the user."""
+		return self._first_name
+
+	@property
+	def last_name(self) -> str | None:
+		"""The last name of the user."""
+		return self._last_name
+
+	@property
+	def title(self) -> str | None:
+		"""The title of the user."""
+		return self._title
+
+	@property
+	def company(self) -> str | None:
+		"""The user's company."""
+		return self._company
+
+	@property
+	def address(self) -> str | None:
+		"""The user's address."""
+		return self._address
+
+	@property
+	def zip(self) -> int | None:
+		"""The user's zip code."""
+		return self._zip
+
+	@property
+	def city(self) -> str | None:
+		"""The user's city."""
+		return self._city
+
+	@property
+	def state(self) -> str | None:
+		"""The user's state."""
+		return self._state
+
+	@property
+	def country(self) -> str | None:
+		"""The user's country."""
+		return self._country
+
+	@property
+	def date_format(self) -> str | None:
+		"""The date format for the user. Determines the browser date's display."""
+		return self._date_format
+
+	@property
+	def time_zone(self) -> str | None:
+		"""The user's time zone."""
+		return self._time_zone
+
+	@property
+	def language(self) -> str | None:
+		"""The user's language. Either 'en' or 'de'."""
+		return self._language
+
+	@property
+	def phone(self) -> str | None:
+		"""The user's phone number."""
+		return self._phone
+
+	@property
+	def skills(self) -> str | None:
+		"""The user's skills."""
+		return self._skills
+
+	@property
+	def registry_date(self) -> datetime | None:
+		"""The datetime the user registered."""
+		return self._registry_date
+
+	@property
+	def last_login_date(self) -> datetime | None:
+		"""The datetime the user last logged in."""
+		return self._last_login_date
+
+	@property
+	def status(self) -> str | None:
+		"""The status of the user account."""
+		return self._status
+
+	def load(self):
+		"""Loads the rest of the user's data. When a user is fetched using 
+		`Codebeamer.get_users` only the ID, Name, and Email of the user are retrieved. 
+		This prevents a lot of extra data that's not needed from being sent. Thus, 
+		this method exists to flush out the rest of the user information if it is 
+		needed."""
+		if self.registry_date:
+			logger.info('User already loaded, ignoring...')
+			return
+		user_data: dict[str, Any] = self._client.get(f'users/{self.id}')
+		self._first_name = user_data.get('firstName')
+		self._last_name = user_data.get('lastName')
+		self._title = user_data.get('title')
+		self._company = user_data.get('company')
+		self._address = user_data.get('address')
+		self._zip = user_data.get('zip')
+		self._city = user_data.get('city')
+		self._state = user_data.get('state')
+		self._country = user_data.get('country')
+		self._date_format = user_data.get('dateFormat')
+		self._time_zone = user_data.get('timeZone')
+		self._language = user_data.get('language')
+		self._phone = user_data.get('phone')
+		self._skills = user_data.get('skills')
+		self._registry_date = datetime.strptime(user_data.get('registryDate'), '%Y-%m-%dT%H:%M:%S.%f')
+		self._last_login_date = datetime.strptime(user_data.get('lastLoginDate'), '%Y-%m-%dT%H:%M:%S.%f')
+		self._status = user_data.get('status')
